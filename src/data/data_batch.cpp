c8bc46f9bbec8344ccbf616a1219802467c1f1f4
Bobbi Yogatama <bwyogatama@gmail.com>
2026-04-14 14:40:13 -0500

    feat(01-02): rewrite data_batch.cpp with new 3-class type system
    - Non-template method bodies: constructor, get_batch_id, subscribe/unsubscribe, get_subscriber_count
    - Private data accessors: get_current_tier, get_data, get_memory_space (with null check), set_data
    - Explicit template instantiations for read_only_data_batch and mutable_data_batch with both shared_ptr and unique_ptr
    - Removed synchronized_data_batch references and gpu_data_representation include

--- src/data/data_batch.cpp
+++ src/data/data_batch.cpp
@@ -16,134 +16,25 @@
  */
 
 #include <cucascade/data/data_batch.hpp>
-#include <cucascade/data/gpu_data_representation.hpp>
 
 namespace cucascade {
 
-// =============================================================================
-// data_batch (inner) implementation
-// =============================================================================
+// ========== data_batch implementation ==========
 
-synchronized_data_batch::data_batch::data_batch(uint64_t batch_id,
-                                                std::unique_ptr<idata_representation> data)
+data_batch::data_batch(uint64_t batch_id, std::unique_ptr<idata_representation> data)
   : _batch_id(batch_id), _data(std::move(data))
 {
 }
 
-uint64_t synchronized_data_batch::data_batch::get_batch_id() const { return _batch_id; }
+uint64_t data_batch::get_batch_id() const { return _batch_id; }
 
-memory::Tier synchronized_data_batch::data_batch::get_current_tier() const
-{
-  return _data->get_current_tier();
-}
-
-idata_representation* synchronized_data_batch::data_batch::get_data() const { return _data.get(); }
-
-cucascade::memory::memory_space* synchronized_data_batch::data_batch::get_memory_space() const
-{
-  if (_data == nullptr) { return nullptr; }
-  return &_data->get_memory_space();
-}
-
-void synchronized_data_batch::data_batch::set_data(std::unique_ptr<idata_representation> data)
-{
-  _data = std::move(data);
-}
-
-// =============================================================================
-// read_only_data_batch implementation
-// =============================================================================
-
-synchronized_data_batch::read_only_data_batch
-synchronized_data_batch::read_only_data_batch::from_mutable(mutable_data_batch&& rw)
-{
-  auto* parent = rw.parent_;
-  auto* mtx    = rw.lock_.mutex();
-  rw.lock_.unlock();
-  std::shared_lock<std::shared_mutex> lock(*mtx);
-  return read_only_data_batch(parent, std::move(lock));
-}
-
-// =============================================================================
-// mutable_data_batch implementation
-// =============================================================================
-
-synchronized_data_batch::mutable_data_batch
-synchronized_data_batch::mutable_data_batch::from_read_only(read_only_data_batch&& ro)
-{
-  auto* parent = ro.parent_;
-  auto* mtx    = ro.lock_.mutex();
-  ro.lock_.unlock();
-  std::unique_lock<std::shared_mutex> lock(*mtx);
-  return mutable_data_batch(parent, std::move(lock));
-}
-
-// =============================================================================
-// synchronized_data_batch implementation
-// =============================================================================
-
-synchronized_data_batch::synchronized_data_batch(uint64_t batch_id,
-                                                 std::unique_ptr<idata_representation> data)
-  : _batch(batch_id, std::move(data))
-{
-}
-
-synchronized_data_batch::synchronized_data_batch(synchronized_data_batch&& other)
-  : _batch(std::move(other._batch))
-{
-}
-
-synchronized_data_batch& synchronized_data_batch::operator=(synchronized_data_batch&& other)
-{
-  if (this != &other) { _batch = std::move(other._batch); }
-  return *this;
-}
-
-// Lock acquisition (blocking)
-
-synchronized_data_batch::read_only_data_batch synchronized_data_batch::get_read_only()
-{
-  std::shared_lock<std::shared_mutex> lock(_rw_mutex);
-  return read_only_data_batch(this, std::move(lock));
-}
-
-synchronized_data_batch::mutable_data_batch synchronized_data_batch::get_mutable()
-{
-  std::unique_lock<std::shared_mutex> lock(_rw_mutex);
-  return mutable_data_batch(this, std::move(lock));
-}
-
-// Lock acquisition (non-blocking)
-
-std::optional<synchronized_data_batch::read_only_data_batch>
-synchronized_data_batch::try_get_read_only()
-{
-  std::shared_lock<std::shared_mutex> lock(_rw_mutex, std::try_to_lock);
-  if (!lock.owns_lock()) { return std::nullopt; }
-  return read_only_data_batch(this, std::move(lock));
-}
-
-std::optional<synchronized_data_batch::mutable_data_batch>
-synchronized_data_batch::try_get_mutable()
-{
-  std::unique_lock<std::shared_mutex> lock(_rw_mutex, std::try_to_lock);
-  if (!lock.owns_lock()) { return std::nullopt; }
-  return mutable_data_batch(this, std::move(lock));
-}
-
-// Immutable field (lock-free)
-
-uint64_t synchronized_data_batch::get_batch_id() const { return _batch._batch_id; }
-
-// Subscriber count
-
-bool synchronized_data_batch::subscribe()
+bool data_batch::subscribe()
 {
   _subscriber_count.fetch_add(1, std::memory_order_relaxed);
   return true;
 }
 
-void synchronized_data_batch::unsubscribe()
+void data_batch::unsubscribe()
 {
   size_t prev = _subscriber_count.fetch_sub(1, std::memory_order_relaxed);
   if (prev == 0) {
@@ -152,24 +43,33 @@ void synchronized_data_batch::unsubscribe()
   }
 }
 
-size_t synchronized_data_batch::get_subscriber_count() const
+size_t data_batch::get_subscriber_count() const
 {
   return _subscriber_count.load(std::memory_order_relaxed);
 }
 
-// Clone
+// ========== data_batch private data accessors ==========
 
-std::shared_ptr<synchronized_data_batch> synchronized_data_batch::clone(
-  uint64_t new_batch_id, rmm::cuda_stream_view stream)
+memory::Tier data_batch::get_current_tier() const { return _data->get_current_tier(); }
+
+idata_representation* data_batch::get_data() const { return _data.get(); }
+
+memory::memory_space* data_batch::get_memory_space() const
 {
-  std::shared_lock<std::shared_mutex> lock(_rw_mutex);
-
-  if (_batch._data == nullptr) {
-    throw std::runtime_error("Cannot clone: data is null");
-  }
-
-  auto cloned_data = _batch._data->clone(stream);
-  return std::make_shared<synchronized_data_batch>(new_batch_id, std::move(cloned_data));
+  if (_data == nullptr) { return nullptr; }
+  return &_data->get_memory_space();
 }
+
+void data_batch::set_data(std::unique_ptr<idata_representation> data)
+{
+  _data = std::move(data);
+}
+
+// ========== Explicit template instantiations ==========
+
+template class read_only_data_batch<std::shared_ptr<data_batch>>;
+template class read_only_data_batch<std::unique_ptr<data_batch>>;
+template class mutable_data_batch<std::shared_ptr<data_batch>>;
+template class mutable_data_batch<std::unique_ptr<data_batch>>;
 
 }  // namespace cucascade
